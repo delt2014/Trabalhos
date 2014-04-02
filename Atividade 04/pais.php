314f4fb3d6ab034170cfa748d28a4447d7c6345d
leandrostpr <leandrostpr@gmail.com>
2014-04-01 23:52:31 -0300

    Parte 02

--- Atividade 04/pais.php
+++ Atividade 04/pais.php
@@ -11,18 +11,21 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-             class pais{
+             class Pais{
             
                  private $código_ISO;
                  private $nome;
                  private $populacao;
                  private $dimensao;
-                 private $paisFronteira;
+                 public $fronteiras = array();
+				 public $msg = null;
+				 public $densidade;
                          
                  function __construct($código_ISO, $nome, $dimensao) {
                      $this->código_ISO = $código_ISO;
                      $this->nome = $nome;
                      $this->dimensao = $dimensao;
+					 
                  }
 
                  public function getCódigo_ISO() {
@@ -56,12 +59,66 @@ and open the template in the editor.
                  public function setDimensao($dimensao) {
                      $this->dimensao = $dimensao;
                  }
+				 
+                 public function setFronteiras(Pais $fronteiras) {
+                     $this->fronteiras = $fronteiras;
+                 }
 
-                          
-             
-            
-            
-           }
+
+
+				 public function verificarExistencia($código_ISO)
+                 {
+					if ($this->código_ISO == $código_ISO)
+                    {
+					   $msg = "País já cadastrado.";
+					   return $msg;
+					}
+
+                    else
+                    {
+                        $msg = "País cadastrado com sucesso.";
+                    }
+										
+					return $msg;
+				}
+
+                public function verificarFronteiras($fronteiras)
+                {
+                    if ($fronteiras->fronteiras != NULL )
+                    {
+                        $msg = "País com fronteiras.";  
+                    }
+
+                    else
+                    {
+                        $msg = "País sem fronteiras.";
+                    }
+                    
+                    return $msg;
+                }
+
+				public function verificarDensidade($populacao, $dimensao)
+                {
+					$densidade = $populacao / $dimensao;
+					return $densidade;
+				}
+				
+				
+                public function listarVizinhos(Pais $p1, Pais $p2)
+                {
+					foreach ($p1 -> lista as $key => $value) 
+					{
+                       if($p1-> lista[$key] == $p2-> lista[$key])
+					   {
+                           echo "Lista de Paises Vizinhos: ";
+						   echo $p1-> lista[$key];
+						}	
+                }
+
+				
+			}
+ 
+        
         ?>
     </body>
 </html>
